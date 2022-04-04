1c5fda900238eb495d71eef559d3ed52fd7e2f95
RMIT-Matt <95987351+RMIT-Matt@users.noreply.github.com>
2022-04-04 22:18:59 +1000

    Tilemap and move and slide migration

--- Game/Src/Actors/Player.gd
+++ Game/Src/Actors/Player.gd
@@ -1,32 +1,23 @@
 extends ActorBase
 
-
-onready var raycast = $RayCast2D
-
-
-func _physics_process(delta: float) -> void:
-	var move_vec = Vector2()
-	if Input.is_action_pressed("move_up"):
-		move_vec.y -= 1
-	if Input.is_action_pressed("move_down"):
-		move_vec.y += 1
-	if Input.is_action_pressed("move_left"):
-		move_vec.x -= 1
-	if Input.is_action_pressed("move_right"):
-		move_vec.x += 1
+func _physics_process(_delta: float) -> void:
 	
-	move_vec = move_vec.normalized()
-	move_and_collide(move_vec * MOVE_SPEED * delta)
+	_get_dir()
+	velocity = move_and_slide(velocity,Vector2(0,1))
+	velocity.x = lerp(velocity.x,0,0.2)
+	velocity.y = lerp(velocity.y,0,0.2)
 	
-	#rotating the mans to the mouse
+	
 	var look_vec = get_global_mouse_position() - global_position
 	global_rotation = atan2(look_vec.y, look_vec.x)
-	
-	
 
-#Another comment, please delete me
-
-#Just touching up everything that was conflicting
-
-#aaaaaaaaahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-
+#Making this function as we might be able to set up cutscenes later to use it.
+func _get_dir():
+	if Input.is_action_pressed("move_right") and !Input.is_action_pressed("move_left"):
+		velocity.x = move_speed
+	elif Input.is_action_pressed("move_left") and !Input.is_action_pressed("move_right"):
+		velocity.x = -move_speed
+	elif Input.is_action_pressed("move_down") and !Input.is_action_pressed("move_up"):
+		velocity.y = move_speed
+	elif Input.is_action_pressed("move_up") and !Input.is_action_pressed("move_down"):
+		velocity.y = -move_speed
