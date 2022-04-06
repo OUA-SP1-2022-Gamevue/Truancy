eb41f9f97e28006b64d3b34ac478dd0a1c9bd611
RMIT-Matt <95987351+RMIT-Matt@users.noreply.github.com>
2022-04-07 06:16:35 +1000

    enemy patrol foundations created

--- Game/Src/Actors/Enemy.gd
+++ Game/Src/Actors/Enemy.gd
@@ -1,21 +1,54 @@
 extends ActorBase
 
 var origin = Vector2()
-#speed variable set as constant, this controls move speed
-var direction = 1
+export(int,"Still","Down","Up","Right","Left") var direction
 export var stray = 60 #how far they will stray from the origin
 
+
 func _ready():
-	$Sprite.rotation_degrees = 90
 	origin.x = position.x
 	origin.y = position.y
-	velocity.y = move_speed
-
+	_get_dir()
+		
 func _physics_process(delta):
-	move_and_slide(velocity * direction)
+	_get_dir()
+	move_and_slide(velocity)
 	if origin.y <= position.y-stray:
-		direction = -1
-		$Sprite.rotation_degrees = 270
+		_get_flip()
 	elif origin.y >= position.y+stray:
-		direction = 1
+		_get_flip()
+	if origin.x <= position.x-stray:
+		_get_flip()
+	elif origin.x >= position.x+stray:
+		_get_flip()
+
+func _get_dir():
+	if direction == 1:
 		$Sprite.rotation_degrees = 90
+		velocity.y = move_speed
+	elif direction == 2:
+		$Sprite.rotation_degrees = 270
+		velocity.y = -move_speed
+	elif direction == 3:
+		$Sprite.rotation_degrees = 0
+		velocity.x = move_speed
+	elif direction == 4:
+		$Sprite.rotation_degrees = 180
+		velocity.x = -move_speed
+		
+func _get_flip():
+	if direction == 1:
+		direction = 2
+	elif direction == 2:
+		direction = 1
+	elif direction == 3:
+		direction = 4
+	elif direction == 4:
+		direction = 3
+	$Sprite.rotation_degrees -= 180
+
+func _on_Area2D_body_entered(body):
+	if body.name == "TileMap" or body.get_collision_layer() == 2 or body.get_collision_layer() == 1:
+		_get_flip()
+	#if body.name == "Player":
+	#	get_tree().change_scene("res://Game/Src/Levels/TestLevel.tscn")
