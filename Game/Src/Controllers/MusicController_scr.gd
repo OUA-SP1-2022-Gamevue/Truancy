93be2273c7f5e92ad63a613b560c720636eec8f6
Sky Velleity <velleity@outlook.com.au>
2022-04-27 16:51:24 +1000

    Added comments to MusicController

--- Game/Src/Controllers/MusicController_scr.gd
+++ Game/Src/Controllers/MusicController_scr.gd
@@ -1,6 +1,5 @@
 extends Control
 
-
 #First we're setting up our instance of an audio player, called _player
 onready var _player = $AudioStreamPlayer
 
@@ -8,20 +7,33 @@ onready var _player = $AudioStreamPlayer
 var _currentTrack : String
 var _queuedTrack : String
 
-#eh I'll comment the rest later
-var isMuted = false
+#Now a bool to check if we are currently muted or not:
+var _isMuted = false
 
 #Internal fuctions:
+#I've pulled a sneaky one on you, so far everything has had a "_" before the name
+#That's because they're internal, and we don't want other classes playing with them,
+#We want everything else to use getters and setters so we never loose track of our state
 
-func _play(track_url : String):
-	if !isMuted:
-		var track = load(track_url)
+#This is an internal function that will play the _currentTrack,
+#it should only be called in cases where we have just set _currentTrack
+func _play():
+	#check if we are not(!) muted, otherwise do nothing
+	if !_isMuted:
+		#loading the track at the URL
+		var track = load(_currentTrack)
+		#telling the player to stream in the file
 		_player.stream = track
+		#now playing it
 		_player.play()
 		pass
 
-func stop():
-	_player.stop()
+#After a current track is finished, this needs to check if there is anything queued,
+#And if so, move it forwards into _currentTrack and play it, otherwise, do nothing
+#other than repeat _currentTrack
+func progressQueue():
+	##to be called once current playback ends, progress to next song in queue
+	pass
 	
 	
 	
@@ -29,46 +41,76 @@ func stop():
 
 
 ##External Functions:
+#These are the only things we want to expose to the outside world, to make things nice and clean.
+#We'll keep track of everything in here, so all anything else needs to worry about is if it wants to queue a song,
+#Or if it wants it to play immediatly
 
+#This one's for queueing a track
 func queueTrack( nextTrack : String ):
+	#we get nextTrack from the caller, and set it to the queue, overwriting whatever's there
 	_queuedTrack = nextTrack
+	#We check if there's anything in _currentTrack, and if not, we assign the queue to it, play it, and clear the queue
 	if (_currentTrack == ""):
 		_currentTrack = _queuedTrack
-		_play(_currentTrack)
+		_queuedTrack = ""
+		_play()
 
+#We clear the queue, and play the track we were passed
 func PlayTrack( nextTrack : String ):
 	_queuedTrack = ""
 	_currentTrack = nextTrack
-	_play(_currentTrack)
+	_play()
 
 
-func progressQueue():
-	##to be called once current playback ends, progress to next song in queue
-	pass
+
+
 	
+#does exactly what it says on the box
+func stop():
+	_player.stop()
+
 	
 #External Mute access
+
+#If something calls this, it mutes itself instantly
 func mute():
-	isMuted = true
+	_isMuted = true
 	stop()
 
+#Toggle's mute. Slightly redundant, but this one is too sleepy to determine what is needed, please ignore it.
+
 func toggleMute():
-	print("Toggling Mute")
-	isMuted = !isMuted
-	if isMuted:
-		print("Stopping")
+	#Invert the mute bool
+	_isMuted = !_isMuted
+	#check if we're now muted, if so, stop
+	if _isMuted:
 		stop()
+	#Otherwise, resume what we had.
 	else:
-		print("Can't stop the vibes!")
-		##check for curre
+		if _currentTrack != "":
+			_play()
+		#If we had nothing, but something is queued, bring it in and play
+		elif _queuedTrack != "":
+			_currentTrack = _queuedTrack
+			_queuedTrack = ""
+			_play()
 		pass
+		
+#Something so other scripts can check if we're muted or not
+#returns a bool for easy access
+func areWeMuted() -> bool:
+	return _isMuted
+	
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Not sure if there's anything to use this for, but can't hurt to have it
-
+	#we are currently dependant on the first scene that loads to kick things off,
+	#or anything else that happens to have the occasion to queue some audio
+	
+	
 
 #If anyone needs to use the following, you're doing it wrong:
 
