c2330101e6e71b0dfe8029ddcad776af83764787
Sky Velleity <velleity@outlook.com.au>
2022-04-27 15:59:13 +1000

    Implement quequing
    Just saving things now so I have a clean slate to fall back to

--- Game/Src/Controllers/MusicController_scr.gd
+++ Game/Src/Controllers/MusicController_scr.gd
@@ -1,21 +1,49 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+#First we're setting up our instance of an audio player, called _player
 onready var _player = $AudioStreamPlayer
 
+#Here we will keep track of the current track being played, stored as the resource URL in a string
+var currentTrack : String
+var queuedTrack : String
 
-func play(track_url : String):
-	var track = load(track_url)
-	_player.stream = track
-	_player.play()
-	pass
+#eh I'll comment the rest later
+var isMuted = false
+
+
+func _play(track_url : String):
+	if !isMuted:
+		var track = load(track_url)
+		_player.stream = track
+		_player.play()
+		pass
 
 func stop():
 	_player.stop()
+	
+func mute():
+	isMuted = true
+	stop()
+
+func toggleMute():
+	isMuted = !isMuted
+	if isMuted:
+		stop()
+	else:
+		##read current queue'd track, play
+		pass
+
+func queueTrack( nextTrack : String ):
+	queuedTrack = nextTrack
+	if (currentTrack == ""):
+		currentTrack = queuedTrack
+		_play(currentTrack)
+
+func PlayTrack( nextTrack : String ):
+	queuedTrack = ""
+	currentTrack = nextTrack
+	_play(currentTrack)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
