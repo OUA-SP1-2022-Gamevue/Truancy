cb77899f5f6089fe91f3ea2db7f3132b432d3507
Sky Velleity <velleity@outlook.com.au>
2022-04-27 16:27:55 +1000

    Added mute button to menu as test

--- Game/Src/Controllers/MusicController_scr.gd
+++ Game/Src/Controllers/MusicController_scr.gd
@@ -5,12 +5,13 @@ extends Control
 onready var _player = $AudioStreamPlayer
 
 #Here we will keep track of the current track being played, stored as the resource URL in a string
-var currentTrack : String
-var queuedTrack : String
+var _currentTrack : String
+var _queuedTrack : String
 
 #eh I'll comment the rest later
 var isMuted = false
 
+#Internal fuctions:
 
 func _play(track_url : String):
 	if !isMuted:
@@ -22,33 +23,54 @@ func _play(track_url : String):
 func stop():
 	_player.stop()
 	
+	
+	
+
+
+
+##External Functions:
+
+func queueTrack( nextTrack : String ):
+	_queuedTrack = nextTrack
+	if (_currentTrack == ""):
+		_currentTrack = _queuedTrack
+		_play(_currentTrack)
+
+func PlayTrack( nextTrack : String ):
+	_queuedTrack = ""
+	_currentTrack = nextTrack
+	_play(_currentTrack)
+
+
+func progressQueue():
+	##to be called once current playback ends, progress to next song in queue
+	pass
+	
+	
+#External Mute access
 func mute():
 	isMuted = true
 	stop()
 
 func toggleMute():
+	print("Toggling Mute")
 	isMuted = !isMuted
 	if isMuted:
+		print("Stopping")
 		stop()
 	else:
-		##read current queue'd track, play
+		print("Can't stop the vibes!")
+		##check for curre
 		pass
 
-func queueTrack( nextTrack : String ):
-	queuedTrack = nextTrack
-	if (currentTrack == ""):
-		currentTrack = queuedTrack
-		_play(currentTrack)
 
-func PlayTrack( nextTrack : String ):
-	queuedTrack = ""
-	currentTrack = nextTrack
-	_play(currentTrack)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass # Not sure if there's anything to use this for, but can't hurt to have it
 
+
+#If anyone needs to use the following, you're doing it wrong:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
