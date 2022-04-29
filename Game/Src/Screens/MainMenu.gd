6c18e6a2ba999cfe0ac3aca9e9e2fc1dc28f9e33
RMIT-Matt <95987351+RMIT-Matt@users.noreply.github.com>
2022-04-30 04:43:32 +1000

    Added visioncone update and renamed folders

---
+++ Game/Src/Screens/MainMenu.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _ready():
+	MusicController.queueTrack("res://game/assets/audio/Level1S0.mp3")
--- Game/Src/Screens/MainMenu_scr.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-
-func _ready():
-	MusicController.queueTrack("res://Game/Assets/Audio/Level1S0.mp3")
